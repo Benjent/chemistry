4bdf0cb98039e21dc46d2975b440816790f7314f
Benjent <Benjent@hotmail.fr>
2018-03-16 22:04:17 +0100

    Create a fake profile model

---
+++ assets/public/js/model_Profile.js
@@ -0,0 +1,7 @@
+function Profile(details) {
+    this.firstName          = details.firstName;
+    this.lastName           = details.lastName;
+    this.birth              = details.birth;
+    this.instruments        = details.instruments;
+    this.description        = details.description;
+}
