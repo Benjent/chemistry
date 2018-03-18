0af028ad444f94fa3279954f1ee30883e15c5ff1
Benjent <Benjent@hotmail.fr>
2018-03-18 16:46:48 +0100

    Close previous modal on new modal open

--- assets/public/js/utils.js
+++ assets/public/js/utils.js
@@ -1,13 +1,22 @@
+var lastOpenedModalId = null;
+
 function toggleElement(element, action) {
 
     var id = "#" + element;
     var element = document.querySelector(id);
 
+    if(lastOpenedModalId) {
+        // Close previously opened modal
+        document.querySelector(lastOpenedModalId).attributes.state.value = "closed";
+    }
+
     if(action == "open" && element.attributes.state.value != "open") {
         // Open element
         element.attributes.state.value = "open";
+        lastOpenedModalId = id;
     } else if (action == "close" && element.attributes.state.value != "closed") {
         // Close element
         element.attributes.state.value = "closed";
+        lastOpenedModalId = null;
     }
 }
