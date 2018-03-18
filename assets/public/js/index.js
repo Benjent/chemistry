bdbf20baef7623ec1271c7a2587088015955f0bf
Benjent <Benjent@hotmail.fr>
2018-03-18 15:04:16 +0100

    Check password similarity

--- assets/public/js/index.js
+++ assets/public/js/index.js
@@ -5,3 +5,9 @@ function Profile(details) {
     this.instruments        = details.instruments;
     this.description        = details.description;
 }
+
+function checkPasswordSimilarity() {
+
+    document.querySelector("#signUpMessage").innerHTML = document.querySelector("#signUpPassword").value != document.querySelector("#signUpConfirmPassword").value ? "Passwords don't match." : "";
+
+}
